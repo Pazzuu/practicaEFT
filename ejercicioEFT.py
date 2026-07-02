c517045758a62fbac208697da070a8336210b265
Pazzuu <mari.salinasa@duocuc.cl>
2026-07-02 15:33:09 -0400

    Update ejercicioEFT.py

--- ejercicioEFT.py
+++ ejercicioEFT.py
@@ -1,40 +1,63 @@
-productos = {
-'P001': ['Capuccino Clásico', 'cafe', 'mediano', 'entera', False],
-'P002': ['Latte Vainilla', 'cafe', 'grande', 'descremada', True],
-'P003': ['Té Verde Helado', 'te', 'mediano', 'sin leche', False],
-'P004': ['Mocha Avellana', 'cafe', 'grande', 'entera', True],
-'P005': ['Chocolate Caliente', 'bebida', 'chico', 'entera', False],
-'P006': ['Té Chai Latte', 'te', 'mediano', 'descremada', True]
-}
-ventas = {
-'P001': [2500, 15],
-'P002': [3200, 0],
-'P003': [2800, 10],
-'P004': [3500, 4],
-'P005': [2200, 7],
-'P006': [3100, 9]
-}
-
 def mostrar_menu():
-    print("===== MENÚ PRINCIPAL =====")
-    print("1. Stock por categoria")
+    print("=========== Menú Principal =========")
+    print("1. Stock por catergoría")
     print("2. Búsqueda de productos por rango de precio")
     print("3. Actualizar precio de producto")
     print("4. Agregar producto")
     print("5. Eliminar producto")
     print("6. Salir")
-    print("==========================")
+    print("===================================")
 
-def ingresar_opcion():
+def leer_opcion():
     while True:
         try:
-            op = int(input("Ingrese opcion que desee realizar"))
-            if op < 1 or op > 6:
-                raise ValueError
-            else:
+            op = int(input("Ingrese una opción: "))
+            if 1 <= op <= 6
                 return op
+            else:
+                print("Debe seleccionar una opción válida")
         except:
-            print("Error: Ingrese un número entero del 1 al 6")
+            print("Debe seleccionar una opción válida")
+
+def validar_codigo(codigo, productos):
+    return codigo.strip() != "" and codigo.strip().upper() not in productos:
+        
+def validar_nombre(nombre_producto):
+    return nombre_producto.strip() != ""
+
+def validar_categoria(categoria):
+    return categoria.strip() != ""
+
+def validar_tamano(tamano):
+    return tamano.strip().lower() in ['chico', 'mediano', 'grande']
+
+def validar_leche(tipo_leche):
+    return tipo_leche.strip() != ""
+
+def validar_temporada(es_temporada):
+    return tamano.strip().lower() in ['s','n']
+
+def validar_precio(precio):
+    if precio.isdigit():
+        validar = int(precio)
+        return validar > 0
+    return False
+
+def validar_stock(stock_disponible):
+    if stock_disponible.isdigit():
+        validar = int(stock_disponible)
+        return validar >= 0
+    return False
+
+def agregar_producto(productos):
+    codigo = input("")
+    correcto = validar_codigo(codigo)
+    if not correcto:
+        print("No p")
+        return
+    
+    
+
 
 
         
